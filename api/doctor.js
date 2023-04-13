ca647ffda43cd68179b376a710a2771d9726a67c
steinskeeper <dpelpsykongroo@gmail.com>
2023-04-13 10:17:15 +0530

    list of orgs

--- api/doctor.js
+++ api/doctor.js
@@ -5,14 +5,22 @@ var multer = require("multer");
 const storage = require("../utils/multerStorage");
 
 const grantAccess = require("../utils/verifytoken");
+const Doctor = require("../models/Doctor");
 
 router.get("/test", (req, res) => {
 	res.status(200).json({ status: "hello" });
 });
 
+router.get("/getorgs", grantAccess(), async (req, res) => {
+	const userid = req.user.id;
+	const doctor = await Doctor.findById(userid);
+	const orgs = doctor.organisations;
+	res.status(200).json({ status: "success", organisations: orgs });
+});
+
 router.get("/portfolio", grantAccess(), (req, res) => {
 	const userid = req.user.id;
-	
+
 	res.status(200).json({ status: "hello" });
 });
 
