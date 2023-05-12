471ee2e6d468ab463a4198e5ddb241832c44ec2c
yajatvishwakk <yajat472001@gmail.com>
2023-05-12 06:35:41 +0530

    vitals

--- utils/vitalsParser.js
+++ utils/vitalsParser.js
@@ -1,41 +1,55 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+const csv = require("csv-parser");
+const fs = require("fs");
+const moment = require("moment");
 
-function vitalParser(filePath) {
+function vitalParser(filePath, vitalStartTime) {
   return new Promise((resolve, reject) => {
     const vitals = [];
     const timestamp = [];
-
+    const startTime = moment(vitalStartTime, "DD/MM/YYYY HH:mm:ss");
     fs.createReadStream(filePath)
       .pipe(csv())
-      .on('data', (data) => {
+      .on("data", (data) => {
         const keys = Object.keys(data);
-        timestamp.push(parseInt(data['Timestamp']));
 
-        for (let i = 1; i < keys.length; i++) {
-          const key = keys[i];
-          const name = key.split('(')[0].trim();
-          const unit = key.match(/\(([^)]+)\)/)[1];
-          const value = parseInt(data[key]);
+        if (
+          moment(data["Timestamp"], "DD/MM/YYYY HH:mm:ss").isSameOrAfter(
+            startTime
+          )
+        ) {
+          timestamp.push(
+            moment(data["Timestamp"], "DD/MM/YYYY HH:mm:ss").diff(
+              startTime,
+              "seconds"
+            )
+          );
+          for (let i = 1; i < keys.length; i++) {
+            const key = keys[i];
+            const name = key.split("(")[0].trim();
+            const unit = key.match(/\(([^)]+)\)/)[1];
+            const value = parseInt(data[key]);
 
-          const index = vitals.findIndex((v) => v.name === name && v.unit === unit);
-
-          if (index >= 0) {
-            vitals[index].values.push(value);
-          } else {
-            vitals.push({
-              name,
-              unit,
-              values: [value],
-            });
+            const index = vitals.findIndex(
+              (v) => v.name === name && v.unit === unit
+            );
+            if (index >= 0) {
+              vitals[index].values.push(value);
+            } else {
+              vitals.push({
+                name,
+                unit,
+                values: [value],
+              });
+            }
           }
         }
       })
-      .on('end', () => {
-        const final = {vitals, timestamp};
+      .on("end", () => {
+        console.log(vitals, timestamp);
+        const final = { vitals, timestamp };
         resolve(final);
       })
-      .on('error', (error) => {
+      .on("error", (error) => {
         reject(error);
       });
   });
