0a792b99bceb0fd033f163a1829a3fd3f96f3c5d
yajatvishwakk <yajat472001@gmail.com>
2023-05-12 07:06:15 +0530

    handles no vitalStartTime

--- utils/vitalsParser.js
+++ utils/vitalsParser.js
@@ -6,41 +6,48 @@ function vitalParser(filePath, vitalStartTime) {
   return new Promise((resolve, reject) => {
     const vitals = [];
     const timestamp = [];
-    const startTime = moment(vitalStartTime, "DD/MM/YYYY HH:mm:ss");
+    let startTime = vitalStartTime
+      ? moment(vitalStartTime, "DD/MM/YYYY HH:mm:ss")
+      : null;
     fs.createReadStream(filePath)
       .pipe(csv())
       .on("data", (data) => {
         const keys = Object.keys(data);
-
-        if (
-          moment(data["Timestamp"], "DD/MM/YYYY HH:mm:ss").isSameOrAfter(
-            startTime
-          )
-        ) {
-          timestamp.push(
-            moment(data["Timestamp"], "DD/MM/YYYY HH:mm:ss").diff(
-              startTime,
-              "seconds"
+        if (startTime) {
+          if (
+            moment(data["Timestamp"], "DD/MM/YYYY HH:mm:ss").isSameOrAfter(
+              startTime
             )
-          );
-          for (let i = 1; i < keys.length; i++) {
-            const key = keys[i];
-            const name = key.split("(")[0].trim();
-            const unit = key.match(/\(([^)]+)\)/)[1];
-            const value = parseInt(data[key]);
-
-            const index = vitals.findIndex(
-              (v) => v.name === name && v.unit === unit
+          ) {
+            timestamp.push(
+              moment(data["Timestamp"], "DD/MM/YYYY HH:mm:ss").diff(
+                startTime,
+                "seconds"
+              )
             );
-            if (index >= 0) {
-              vitals[index].values.push(value);
-            } else {
-              vitals.push({
-                name,
-                unit,
-                values: [value],
-              });
+            for (let i = 1; i < keys.length; i++) {
+              const key = keys[i];
+              const name = key.split("(")[0].trim();
+              const unit = key.match(/\(([^)]+)\)/)[1];
+              const value = parseInt(data[key]);
+
+              const index = vitals.findIndex(
+                (v) => v.name === name && v.unit === unit
+              );
+              if (index >= 0) {
+                vitals[index].values.push(value);
+              } else {
+                vitals.push({
+                  name,
+                  unit,
+                  values: [value],
+                });
+              }
             }
+          }
+        } else {
+          if (timestamp.length === 0) {
+            startTime = moment(data["Timestamp"], "DD/MM/YYYY HH:mm:ss");
           }
         }
       })
@@ -54,5 +61,6 @@ function vitalParser(filePath, vitalStartTime) {
       });
   });
 }
+vitalParser("./static/vitals/1.csv");
 
 module.exports = vitalParser;
